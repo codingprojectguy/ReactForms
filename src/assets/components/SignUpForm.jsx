83a98c161ad969e85b7fbbc0b8111d7301370c8d
Home PC <vfxprojectguy@gmail.com>
2023-10-13 13:12:45 -0700

    complete sign up component

---
+++ src/assets/components/SignUpForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import React from "react";
+
+const SignUpForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    console.log();
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: { username },
+            password: { password },
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2>Sign Up!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:{" "}
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default SignUpForm;
